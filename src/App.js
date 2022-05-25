651bb81073410a6c0273fa18c564186f42ed2b17
Paulo Araújo <90206752+86Paulo@users.noreply.github.com>
2022-05-25 22:07:32 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,12 +6,22 @@ const api = {
 }
 
 function App() {
+
+  const dateBuilder = (date) => {
+    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+  }
+
   return (
     <div className="App">
       <main>
         <div className='search-box'>
           <input type="text" className='search-bar' placeholder='Search...'>
           </input>
+        </div>
+        <div>
+          <div className='location-box'>Lisboa</div>
+          <div className='date'>{dateBuilder(new Date())}</div>
         </div>
       </main>
     </div>
